d335129bef64314a3b82ff72cd6bf0957539c765
Rahim00001 <rahimlpt2022@gmail.com>
2024-07-03 18:14:40 +0600

    basic project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "flowbite-react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center'>Hello From Healthcare</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
